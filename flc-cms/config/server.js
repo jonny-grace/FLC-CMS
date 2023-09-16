4514409554e303a0251754bc3aa33578ef9bf97d
jonny-grace <johnlemma9@gmail.com>
2023-09-16 15:26:46 +0300

    babel problem fixed

---
+++ flc-cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3bcf7c066ca680da41389c014b75bb6e'),
+    },
+  },
+});
---
+++ flc-cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '80c16b5c-e93b-4ec8-8b75-5cf22f910440'
+};
